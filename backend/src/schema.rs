a55e69d85526d36a271c487564a6f857e2ce3646
noimin0610 <mii.informationsystems@gmail.com>
2020-09-23 18:37:13 +0900

    #22 tags_timers に主キーを定義して schema に反映

--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -17,6 +17,13 @@ table! {
 }
 
 table! {
+    tags_timers (tag_id, timer_id) {
+        tag_id -> Int4,
+        timer_id -> Int4,
+    }
+}
+
+table! {
     timers (id) {
         id -> Int4,
         uid -> Varchar,
@@ -33,10 +40,13 @@ table! {
 }
 
 joinable!(records -> timers (timer_id));
+joinable!(tags_timers -> tags (tag_id));
+joinable!(tags_timers -> timers (timer_id));
 
 allow_tables_to_appear_in_same_query!(
     records,
     tags,
+    tags_timers,
     timers,
     users,
 );
